2fbf3f3142ad8c1bdb5714361a907a9d2dd94885
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2023-05-21 00:27:29 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WorldDominion.Data;
+
+namespace WorldDominion
+{
+    public class Program
+    {
+        public static void Main(string[] args) //Entire application starts from this file
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment()) //app.= registering a middleware/factory pattern (taking a request)-> resolve or to next middleware
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection(); //loads 
+            app.UseStaticFiles(); //registers request
+
+            app.UseRouting(); //where all routing happens
+
+            app.UseAuthentication(); 
+            app.UseAuthorization();
+
+            RouteConfig.ConfigureRoutes(app); //routes are read in order, 
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
---
+++ RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿namespace WorldDominion
+{
+    public class RouteConfig
+    {
+        public static void ConfigureRoutes(IEndpointRouteBuilder routes) 
+        {
+
+            routes.MapControllerRoute(
+                name: "about",
+                pattern: "about",
+                defaults: new { controller = "Home", action = "About" } //route override
+            ); //instantiating a new object
+
+            routes.MapControllerRoute(
+                name: "contact",
+                pattern: "contact",
+                defaults: new { controller = "Home", action = "Contact" });
+
+            routes.MapControllerRoute( //WITHOUT THIS ROUTE: https://localhost:7150/Home/Privacy ;//WITH THIS ROUTE: https://localhost:7150/privacy
+                name: "privacy",
+                pattern: "privacy",
+                defaults: new { controller = "Home", action = "Privacy" } //route override
+            ); //instantiating a new object
+
+            routes.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
