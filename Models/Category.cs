6dced7f4b1eb2cc97e8105b6addd52d5b77e7f94
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2023-05-21 00:26:12 -0400

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WorldDominion.Models
+{
+    public class Category
+    {
+        public int Id { get; set; } //get & set = accessor 
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
