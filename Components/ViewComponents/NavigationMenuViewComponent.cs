8a6572ecfc3088a43b460e122c9f449fb7230830
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2023-07-29 16:29:18 -0400

    Add files via upload

--- Components/ViewComponents/NavigationMenuViewComponent.cs
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -9,19 +9,21 @@ namespace Mage.Components.ViewComponents
         //to render method
         public IViewComponentResult Invoke()
         {
+            //Authorized = true only shows Menu items if authorized
             var menuItems = new List<MenuItem>
             {
                 new MenuItem { Controller = "Home", Action = "Index", Label = "Home" },
+                new MenuItem { Controller = "Shop", Action = "Index", Label = "Shop"},
                 new MenuItem { Controller = "Categories", Action = "Index", Label = "Categories", DropdownItems = new
                 List<MenuItem> { //creates drop down items in navigation bar
                     new MenuItem{Controller = "Categories", Action = "Index", Label = "List"} ,
                     new MenuItem{Controller = "Categories", Action = "Create", Label = "Create"} ,
-                    } },
+                    }, Authorized = true, AllowedRoles = new List<string> { "Administrator"} },
                 new MenuItem { Controller = "Games", Action = "Index", Label = "Games", DropdownItems = new
                 List<MenuItem> { //creates drop down items in navigation bar
                     new MenuItem{Controller = "Games", Action = "Index", Label = "List"} ,
                     new MenuItem{Controller = "Games", Action = "Create", Label = "Create"} ,
-                    } },
+                    }, Authorized = true, AllowedRoles = new List<string> { "Administrator"} }, 
                 new MenuItem { Controller = "Home", Action = "About", Label = "About" },
                 new MenuItem { Controller = "Home", Action = "Contact", Label = "Contact" },
                 new MenuItem { Controller = "Home", Action = "Privacy", Label = "Privacy" },
