1cc615ee7eb96fc3197337d97438972fb9e04a44
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2023-08-07 17:14:20 -0400

    Add files via upload

--- Components/ViewComponents/NavigationMenuViewComponent.cs
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -11,14 +11,15 @@ namespace Mage.Components.ViewComponents
             {
                 new MenuItem {Controller = "Home", Action = "Index", Label = "Home" },
                 new MenuItem {Controller = "Shop", Action = "Index", Label = "Shop" },
-                new MenuItem {Controller = "Categories", Action = "Index", Label = "Category", DropdownItems = new List<MenuItem>  {
-                    new MenuItem {Controller = "Categories", Action = "Index", Label = "List"},
-                    new MenuItem {Controller = "Categories", Action = "Create", Label = "Create"},
-                }, Authorized = true, AllowedRoles = new List<string>{"Administrator"} },
-                new MenuItem {Controller = "Games", Action = "Index", Label = "Game", DropdownItems = new List<MenuItem>  {
-                    new MenuItem {Controller = "Games", Action = "Index", Label = "List"},
-                    new MenuItem {Controller = "Games", Action = "Create", Label = "Create"},
-                }, Authorized = true, AllowedRoles = new List<string>{"Administrator"} },
+                new MenuItem {Controller = "Shop", Action = "ViewMyCart", Label = "Cart", Authorized = true },
+                new MenuItem {Controller = "Shop", Action = "Orders", Label = "Orders",  Authorized = true },
+                new MenuItem {Controller = "Orders", Action = "Index", Label ="Admin", Authorized = true, AllowedRoles = new List<string>{"Administrator"}, 
+                    DropdownItems = new List<MenuItem>{
+                    new MenuItem {Controller = "Categories", Action = "Index", Label = "Categories"},
+                    new MenuItem {Controller = "Games", Action = "Index", Label = "Games"},
+                    new MenuItem {Controller = "Orders", Action = "Index", Label = "Orders"},
+                    new MenuItem {Controller = "Carts", Action = "Index", Label = "Carts"},
+                } },
                 new MenuItem {Controller = "Home", Action = "About", Label = "About"},
                 new MenuItem {Controller = "Home", Action = "Contact", Label = "Contact"},
                 new MenuItem {Controller = "Home", Action = "Privacy", Label = "Privacy" },
